5b863a8b5138ede9cd533d71f70fdb9b659eac43
Rodrigo <rflores456123@gmail.com>
2021-06-07 14:30:56 -0700

    some manager backend functionality

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const employee = require('./routes/employee')
 const manager = require('./routes/manager')
 
 app.use(express.static('./public'))
+app.use('/manager', manager)
+
+// app.get('/filtering/:id', function(req,res,next){
+//     if(req.params.id === 'Pending') next('/manager')
+//     else next()
+//     let test = function (req,res,next){
+//     }
+// })
 
 app.listen(5051, () => {
     console.log("Port 5051");
