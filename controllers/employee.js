467adb14518e9812150e5ac57c1c77ad86066205
Thunderwizard99 <isaakjensen25@gmail.com>
2021-06-08 13:08:50 -0700

    employee controller/router work

--- controllers/employee.js
+++ controllers/employee.js
@@ -1,3 +1,29 @@
+let { reimbursement } = require('../data/reimbursements.js')
+const mongo = require('mongodb')
+const client = require('../service/connection')
+const service = require('../service/manager')
+
+const getAllByEmployeePending = (req, res) => {
+    service.getAllByEmployeePending(client).then((result) => {
+        if (result) { res.status(200).json({ success: true, data: result }) }
+        else {
+            res.status(404).send('no records found')
+        }
+
+    });
+}
+
+const getAllByEmployeeResolved = (req, res) => {
+    service.getAllByEmployeeResolved(client).then((result) => {
+        if (result) { res.status(200).json({ success: true, data: result }) }
+        else {
+            res.status(404).send('no records found')
+        }
+
+    });
+}
+
 module.exports = {
-    
+    getAllByEmployeePending,
+    getAllByEmployeeResolved
 }
--- routes/employee.js
+++ routes/employee.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    getEmployeeHome
+    getEmployeeHome,
+    getAllByEmployeePending,
+    getAllByEmployeeResolved
 } = require('../controllers/employee');
 
+router.route('/employee/pending/:employee').get(getAllByEmployeePending)
+router.route('/employee/resolved/:employee').get(getAllByEmployeeResolved)
 
 module.exports = router
