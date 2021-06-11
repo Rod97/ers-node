9f8cd2d8ef1c6d15dadc13d6794dbecac0b2e6a4
Rodrigo <rflores456123@gmail.com>
2021-06-11 10:18:11 -0700

    removed unnecessary request files

--- service/connection.js
+++ service/connection.js
@@ -3,10 +3,3 @@ const uri = 'mongodb+srv://rod:FDyx3AW6XMXan%23y@ers-data.g2qxm.mongodb.net/ers-
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true, keepAlive: true });
 client.connect();
 module.exports = client;
-
-// {
-//     "amount":int,
-//     "reason":String,
-//     "employee_id":employee_id,
-//     "status":string
-// }
